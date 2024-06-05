57d2c73e6ef1ff8fd5dbbbdc79b6ba4ca85e4d02
gustavo-specxx <gustagto123@gmail.com>
2024-06-05 02:18:54 -0300

    commit final config

---
+++ app/src/main/java/gs/_ano/app/config/SpringDocConfig.java
@@ -0,0 +1,17 @@
+package gs._ano.app.config;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
